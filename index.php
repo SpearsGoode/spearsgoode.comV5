61e8a1b1595242ded49cfa68ca422e3c35d877e6
SpearsGoode <spearsgoode@protonmail.com>
2022-11-11 13:49:27 -0800

    verify bootstrap works

--- index.php
+++ index.php
@@ -12,12 +12,46 @@
   <link href='./css/main.css' type='text/css' rel='stylesheet'>
 </head>
 
-<body class="container-fluid">
+<body class="container-fluid bg-dark">
 
-  <nav>
+  <nav class="navbar navbar-dark bg-dark sticky-top">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        <img src="./img/Logo.svg" alt="Spears Goode Logo" width="50" height="50">
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+        <div class="offcanvas-header">
+          <h5 class="offcanvas-title" id="offcanvasNavbarLabel"></h5>
+          <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body">
+          <ul class="navbar-nav nav-fill justify-content-end flex-grow-1 pe-3">
+            <li class="nav-item">
+              <a class="nav-link" aria-current="page" href="#">Abstract-Artwork.com</a> <!--FIXME-->
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Armada DAO | Ape Armada</a> <!--FIXME-->
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">aHODLm Indicator</a> <!--FIXME-->
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">SpearsGoode.com</a> <!--FIXME-->
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contact</a> <!--FIXME-->
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </nav>
 
-  <div class="container-lg bg-dark">
+
+  <div class="container-lg bg-light">
     <h1> verify </h1>
   </div>
 
