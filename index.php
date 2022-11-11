524b2d5fa572b2cf55b9adc9a635d0729a6ba629
SpearsGoode <spearsgoode@protonmail.com>
2022-11-10 17:13:15 -0800

    add outline

---
+++ index.php
@@ -0,0 +1,27 @@
+
+<!DOCTYPE html>
+<html  lang="en">
+
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="utf-8">
+  <meta name="description" content=""> <!--FIXME-->
+  <title>SpearsGoode.com - Project Portfolio</title>
+  <link rel="shortcut icon" type="image/svg" href="./img/Logo.svg">
+  <link href='./css/main.css' type='text/css' rel='stylesheet'>
+</head>
+
+<body>
+
+  <nav>
+  </nav>
+
+  <div class="content">
+  </div>
+
+  <footer>
+  </footer>
+
+</body>
+
+</html>
