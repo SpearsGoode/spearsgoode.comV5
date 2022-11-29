838c90786f84175fb513670b5a3a64bfa3de35a8
SpearsGoode <spearsgoode@protonmail.com>
2022-11-28 17:28:15 -0800

    fix meta icon and description

--- index.php
+++ index.php
@@ -6,9 +6,9 @@
 <head>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta charset="utf-8">
-  <meta name="description" content=""> <!--FIXME-->
+  <meta name="description" content="Project Portfolio for some of the more important projects I've worked on."> <!--FIXME-->
   <title>SpearsGoode.com - Project Portfolio</title>
-  <link rel="shortcut icon" type="image/svg" href="./img/Logo.svg">
+  <link rel="shortcut icon" type="image/svg" href="./img/SGlogoV2.svg">
   <link href='./node_modules/bootstrap/dist/css/bootstrap.min.css' type='text/css' rel='stylesheet'>
   <link href='./css/main.css' type='text/css' rel='stylesheet'>
 </head>
